88798df3062b8338a71f453b13d104e104716570
Stephan Kämper <the.tester@seasidetesting.com>
2021-06-03 19:26:23 +0200

    MAkes test more specific

--- test/fd_test.rb
+++ test/fd_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class FdTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Fd::VERSION
+    assert_match %r{\A(\d+\.)+\d+\Z}, ::Fd::VERSION
   end
 
   def test_it_does_something_useful
