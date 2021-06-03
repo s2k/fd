9b9e7fa35e1d8a0a893de5d49e63b9ccab6c1a3f
Stephan Kämper <the.tester@seasidetesting.com>
2021-06-03 19:57:23 +0200

    Replace example test case with real one, cheking running the executale program

--- test/fd_test.rb
+++ test/fd_test.rb
@@ -3,11 +3,12 @@
 require 'test_helper'
 
 class FdTest < Minitest::Test
-  def test_that_it_has_a_version_number
+  def test_has_version_number
     assert_match %r{\A(\d+\.)+\d+\Z}, ::Fd::VERSION
   end
 
-  def test_it_does_something_useful
-    assert true
+  def test_works_with_ascii_input
+    res = `bundle exec exe/fd test/test_data/word-list-ascii-ipsum.txt`
+    assert_equal File.read('test/test_data/expected-word-list-ascii-ipsum-output.txt'), res
   end
 end
