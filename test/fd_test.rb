a4fd1ed95df411d27cc113a5b5a3aea46fd1704d
Stephan Kämper <the.tester@seasidetesting.com>
2021-06-04 19:19:19 +0200

    Adds test for file of all (and only) special characters

--- test/fd_test.rb
+++ test/fd_test.rb
@@ -11,4 +11,9 @@ class FdTest < Minitest::Test
     res = `bundle exec exe/fd test/test_data/word-list-ascii-ipsum.txt`
     assert_equal File.read('test/test_data/expected-word-list-ascii-ipsum-output.txt'), res
   end
+
+  def test_transcribes_invisible_characters_with_given_width
+    res = `bundle exec exe/fd -w 12 test/test_data/special-characters.txt`
+    assert_equal File.read('test/test_data/expected-special-characters-output.txt'), res
+  end
 end
