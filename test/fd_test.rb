fc7f95a4ac2f3fbc00fa2102c6d42964b16f96cb
Stephan Kämper <the.tester@seasidetesting.com>
2022-01-23 13:17:48 +0100

    Moves executable to 'bin' folder
    Signed-off-by: Stephan Kämper <the.tester@seasidetesting.com>

--- test/fd_test.rb
+++ test/fd_test.rb
@@ -14,38 +14,38 @@ class FdTest < Minitest::Test
   end
 
   def test_works_with_ascii_input
-    res = `bundle exec exe/fd test/test_data/word-list-ascii-ipsum.txt`
+    res = `bundle exec bin/fd test/test_data/word-list-ascii-ipsum.txt`
     assert_equal File.read('test/test_data/expected-output-word-list-ascii-ipsum.txt'), res
   end
 
   def test_transcribes_invisible_characters_with_given_width
-    res = `bundle exec exe/fd -w 12 test/test_data/special-characters.txt`
+    res = `bundle exec bin/fd -w 12 test/test_data/special-characters.txt`
     assert_equal File.read('test/test_data/expected-output-special-characters.txt'), res
   end
 
   def test_help_only_prints_help
-    res = `bundle exec exe/fd -h no-such-file`
+    res = `bundle exec bin/fd -h no-such-file`
     assert_equal EXPECTED_HELP_TEXT, res
   end
 
   def test_width_requires_one_parameter
-    res = `bundle exec exe/fd -w 10 test/test_data/word-list-ascii-ipsum.txt`
+    res = `bundle exec bin/fd -w 10 test/test_data/word-list-ascii-ipsum.txt`
     assert_equal File.read('test/test_data/expected-output-word-list-ascii-ipsum.txt'), res
   end
 
   def test_non_integer_width_parameter_causes_help_to_be_displayed
-    res = `bundle exec exe/fd -w f10x test/test_data/word-list-ascii-ipsum.txt`
+    res = `bundle exec bin/fd -w f10x test/test_data/word-list-ascii-ipsum.txt`
     exp = "fd doesn't work, this way:\n#{EXPECTED_HELP_TEXT}"
     assert_equal exp, res
   end
 
   def test_display_very_short_utf_8_text
-    res = `bundle exec exe/fd test/test_data/short-utf-8-text.txt`
+    res = `bundle exec bin/fd test/test_data/short-utf-8-text.txt`
     assert_equal File.read('test/test_data/expected-output-short-utf-8-text.txt'), res
   end
 
   def test_display_long_utf_8_text
-    res = `bundle exec exe/fd test/test_data/long-utf-8-text.txt`
+    res = `bundle exec bin/fd test/test_data/long-utf-8-text.txt`
     assert_equal File.read('test/test_data/expected-output-long-utf-8-text.txt'), res
   end
 end
